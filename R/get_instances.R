4f28530aabc21f4b7c76b77a356a3149508b4f1c
Weinert, Karsten <karsten.weinert@progons.com>
2022-10-31 00:41:04 +0100

    vignettes

--- R/get_instances.R
+++ R/get_instances.R
@@ -3,7 +3,7 @@
 #' @return character of URLs
 #' @export
 get_instances <- function() {
-	fn <- system.file("instances.txt", package="msocial")
+	fn <- system.file("extdata/instances.txt", package="msocial")
 	x <- readLines(fn)
 	x <- Filter(function(y) grepl("^https", y), unique(x))
 	x
---
+++ vignettes/accounts.R
@@ -0,0 +1,10 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+## -----------------------------------------------------------------------------
+#options(
+#	msocial.instance="https://berlin.social/",
+#	msocial.email="me@email.com",
+#	msocial.passw="secret"
+#)
+
